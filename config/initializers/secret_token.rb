fc7f6992f93ba42fb3da86c66c934dac127cf937
Julia Sesbreno <juliasesbreno@Ericks-MacBook-Pro.local>
2017-08-10 17:00:41 +0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookmarks::Application.config.secret_token = 'e487cf2e059dc10e74318fdf4f51fe83c105eda070652d8c75cd4562949e46df7ae8750f4c6f2f494e5afa02305d2f510b782fe92a9f8326cdac802d4373bcde'
